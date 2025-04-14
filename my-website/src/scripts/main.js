e1f2a913716f991e9edb168965e626d41dbe717a
Simon Bouteillon <simon.bouteillon@gmail.com>
2025-04-14 10:56:39 +0200

    nouvelle branche

--- my-website/src/scripts/main.js
+++ my-website/src/scripts/main.js
@@ -1,1 +1,9 @@
-// This file is intentionally left blank.
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.getElementById("title");
+    title.addEventListener("mouseover", () => {
+        title.style.color = "blue";
+    });
+    title.addEventListener("mouseout", () => {
+        title.style.color = "black";
+    });
+});
